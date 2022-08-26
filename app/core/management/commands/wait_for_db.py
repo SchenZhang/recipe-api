dc2d0d0cd19a41b04613873c54f80608ea501745
apple <apple@JamiedeMacBook-Pro.local>
2022-08-26 17:12:00 +0800

    flake down

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,16 +3,17 @@ import time
 from psycopg2 import OperationalError as Psycopg20pError
 from django.db.utils import OperationalError
 
+
 class Command(BaseCommand):
     """django command wait for database"""
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         self.stdout.write('Waiting for databse.......')
-        db_up=False
+        db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                db_up=True
-            except(Psycopg20pError,OperationalError):
+                db_up = True
+            except(Psycopg20pError, OperationalError):
                 self.stdout.write('Database failed, wait for 1 s')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available'))
